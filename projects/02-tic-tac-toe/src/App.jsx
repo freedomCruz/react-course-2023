f4af2a4c7e7bbd204c7a82e116cf12be9711de76
William Rodríguez Cruz <willycruzo@outlook.com>
2023-07-11 10:18:20 -0500

    local storage y eslint

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -2,27 +2,39 @@ import { useState } from 'react'
 import confetti from "canvas-confetti"  
 
 import { Square } from './components/Square.jsx'
+import { WinnerModal } from './components/WinnerModal.jsx'
 import { TURNS } from './constants.js'
 import { checkWinnerFrom } from './logic/board.js'
-import { WinnerModal } from './components/WinnerModal.jsx'
 
 import './App.css'
  
 
 function App() {
+  console.log('render')
   
-  const [board, setBoard ]= useState( 
-    Array(9).fill(null) 
-  )
+  const [board, setBoard ] = useState(() => {
+    console.log('inicializar estado del board')
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
-
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  
+  })
+  
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const checkEndGame = (newBoard) => (
@@ -33,13 +45,16 @@ function App() {
 
     if (board[index] || winner) return
     
-
+// actualizar el tablero y el turno
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
    
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('board', newTurn)
 
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
